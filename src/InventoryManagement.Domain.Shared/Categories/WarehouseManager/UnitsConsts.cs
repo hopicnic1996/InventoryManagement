5771d69d1b4b0b2c23a482bba0c2c032721a99a1
TrungDx <hopicnic1996@gmail.com>
2022-05-04 14:00:33 +0700

    commit prepare

---
+++ src/InventoryManagement.Domain.Shared/Categories/WarehouseManager/UnitsConsts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagement.Categories.WarehouseManager
+{
+	public class UnitsConsts
+	{
+
+		public const int MinNameLength = 1;
+		public const int MaxNameLength = 255;
+
+		public const int MinSymbolLength = 0;
+		public const int MaxSymbolLength = 255;
+
+		public const int MinNoteLength = 0;
+		public const int MaxNoteLength = 255;
+
+	}
+}
---
+++ src/InventoryManagement.Domain/Categories/WarehouseManager/Units.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace InventoryManagement.Categories.WarehouseManager
+{
+    [Table("Unitses")]
+    public class Units : FullAuditedEntity<Guid>, IMultiTenant
+    {
+        public Guid? TenantId { get; set; }
+		[Required]
+		[StringLength(UnitsConsts.MaxNameLength, MinimumLength = UnitsConsts.MinNameLength)]
+		public virtual string Name { get; set; }
+
+		[StringLength(UnitsConsts.MaxSymbolLength, MinimumLength = UnitsConsts.MinSymbolLength)]
+		public virtual string Symbol { get; set; }
+
+		[Required]
+		public virtual int Level { get; set; }
+
+		[StringLength(UnitsConsts.MaxNoteLength, MinimumLength = UnitsConsts.MinNoteLength)]
+		public virtual string Note { get; set; }
+	}
+}
